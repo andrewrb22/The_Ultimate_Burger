5931d3f3200db5ae65fc55b3d66013a70404bbeb
Andrew Rodriguez <andrewrb22@gmail.com>
2020-10-15 18:21:23 -0400

    created index.js, require and install all npm packages

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mysql = require("mysql");
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
