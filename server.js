bdcf55424318358dafab0692b69173076454bd5e
Andrew Rodriguez <andrewrb22@gmail.com>
2020-10-20 23:16:23 -0400

    fixed process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = (process.env.PORT || 3000);
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -28,9 +28,3 @@ app.use(routes);
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
-app.use(timeout(15000));
-app.use(haltOnTimedout);
-
-function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
-}
